49aefa2fcbe29d7d21394e7eb7cf1629c32049b6
rotelstift <rotelstift@gmail.com>
2019-04-24 02:10:33 +0000

    making powerList

--- src/components/Arm.vue
+++ src/components/Arm.vue
@@ -1,28 +1,32 @@
 <template id="arm">
+  <div>
 
   <div id="container">
 
     <div v-for="d in armData" v-if="d.slug === $route.params.slug">
-      <p>{{d}}</p>
+
 
       <div v-for="p in d.parametor" class="blockAreaWrapper">
         <blockArea :item='p' />
-
       </div>
 
     </div>
+  </div>
+  <powerList/>
   </div>
 </template>
 
 <script>
 
   import BlockArea from '@/components/parts/BlockArea'
+  import PowerList from '@/components/parts/PowerList'
   import armData from '../../static/arm.json'
   import mylib from '@/components/util/mylib'
 
   export default {
     components: {
-      blockArea: BlockArea
+      blockArea: BlockArea,
+      powerList: PowerList
     },
     data: function() {
       return {
@@ -51,36 +55,5 @@
     border-width: 1px;
     border-style: dashed;
     padding: 0px;
-
-  }
-  div.inner{
-    margin: 0px;
-    padding: 0px;
-    border: 0;
-    height: 200px;
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-
-  }
-  div.inner p {
-    margin: 0px;
-    width: 200px;
-    height: 100px;
-    font-size: 5em;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-  }
-  div.inner form {
-    height: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  div.inner form input {
-    font-size: 3em;
-    width: 90px;
   }
 </style>
---
+++ src/components/parts/PowerList.vue
@@ -0,0 +1,524 @@
+<template id="PowerList">
+  <table>
+    <colgroup>
+      <col span="1">
+      <col span="7" style="background-color: #ccc">
+      <col span="7">
+      <col span="7" style="background-color: #ccc">
+      <col span="7">
+    </colgroup>
+    <thead>
+      <th>縫う方法</th>
+      <th colspan="7">弱い</th>
+      <th colspan="7">普通</th>
+      <th colspan="7">強い</th>
+      <th colspan="7">最強</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td rowspan="4">糸ほぐし</td>
+        <td colspan="4">-3</td>
+        <td colspan="3">-4</td>
+        <td colspan="2">-6</td>
+        <td colspan="2">-7</td>
+        <td colspan="2">-8</td>
+        <td>-9</td>
+        <td colspan="2">-9</td>
+        <td colspan="2">-10</td>
+        <td colspan="2">-12</td>
+        <td>-13</td>
+        <td colspan="2">-12</td>
+        <td colspan="2">-14</td>
+        <td colspan="2">-16</td>
+        <td>-18</td>
+      </tr>
+      <tr>
+        <td colspan="4"></td>
+        <td colspan="3"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td colspan="2">-6</td>
+        <td colspan="2">-7</td>
+        <td colspan="2">-8</td>
+        <td>-9</td>
+        <td colspan="2">-12</td>
+        <td colspan="2">-14</td>
+        <td colspan="2">-16</td>
+        <td>-18</td>
+        <td colspan="2">-18</td>
+        <td colspan="2">-21</td>
+        <td colspan="2">-24</td>
+        <td>-27</td>
+        <td colspan="2">-24</td>
+        <td colspan="2">-28</td>
+        <td colspan="2">-32</td>
+        <td>-36</td>
+      </tr>
+      <tr>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td rowspan="4">かげんぬい</td>
+        <td id="weak1">3</td>
+        <td colspan="4" id="weak2">4</td>
+        <td colspan="2" id="weak3">5</td>
+        <td>6</td>
+        <td colspan="2">7</td>
+        <td colspan="2">8</td>
+        <td colspan="2">9</td>
+        <td>9</td>
+        <td colspan="2">11</td>
+        <td colspan="2">12</td>
+        <td colspan="2">14</td>
+        <td>12</td>
+        <td colspan="2">14</td>
+        <td colspan="2">16</td>
+        <td colspan="2">18</td>
+      </tr>
+      <tr>
+        <td id="calNextWeak1"></td>
+        <td colspan="4" id="calNextWeak2"></td>
+        <td colspan="2" id="calNextWeak3"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td colspan="4">8</td>
+        <td colspan="2">10</td>
+        <td>12</td>
+        <td colspan="2">14</td>
+        <td colspan="2">16</td>
+        <td colspan="2">18</td>
+        <td>18</td>
+        <td colspan="2">22</td>
+        <td colspan="2">24</td>
+        <td colspan="2">28</td>
+        <td>24</td>
+        <td colspan="2">28</td>
+        <td colspan="2">32</td>
+        <td colspan="2">36</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="4"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+      </tr>
+      <tr>
+        <td rowspan="4">ぬうねらいぬい</td>
+        <td>6</td>
+        <td colspan="2">7</td>
+        <td colspan="2">8</td>
+        <td colspan="2">9</td>
+        <td>12</td>
+        <td>13</td>
+        <td>14</td>
+        <td>15</td>
+        <td>16</td>
+        <td>17</td>
+        <td>18</td>
+        <td>18</td>
+        <td>20</td>
+        <td>21</td>
+        <td>23</td>
+        <td>24</td>
+        <td>26</td>
+        <td>27</td>
+        <td>24</td>
+        <td>26</td>
+        <td>28</td>
+        <td>30</td>
+        <td>32</td>
+        <td>34</td>
+        <td>36</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>12</td>
+        <td colspan="2">14</td>
+        <td colspan="2">16</td>
+        <td colspan="2">18</td>
+        <td>24</td>
+        <td>26</td>
+        <td>28</td>
+        <td>30</td>
+        <td>32</td>
+        <td>34</td>
+        <td>36</td>
+        <td>36</td>
+        <td>40</td>
+        <td>42</td>
+        <td>46</td>
+        <td>48</td>
+        <td>52</td>
+        <td>54</td>
+        <td>48</td>
+        <td>52</td>
+        <td>56</td>
+        <td>60</td>
+        <td>64</td>
+        <td>68</td>
+        <td>72</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td colspan="2"></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td rowspan="4">２倍ぬい</td>
+        <td>12</td>
+        <td>13</td>
+        <td>14</td>
+        <td>15</td>
+        <td>16</td>
+        <td>17</td>
+        <td>18</td>
+        <td>24</td>
+        <td>26</td>
+        <td>28</td>
+        <td>30</td>
+        <td>32</td>
+        <td>34</td>
+        <td>36</td>
+        <td>36</td>
+        <td>39</td>
+        <td>42</td>
+        <td>45</td>
+        <td>48</td>
+        <td>51</td>
+        <td>54</td>
+        <td>48</td>
+        <td>52</td>
+        <td>56</td>
+        <td>60</td>
+        <td>64</td>
+        <td>68</td>
+        <td>72</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>24</td>
+        <td>26</td>
+        <td>28</td>
+        <td>30</td>
+        <td>32</td>
+        <td>34</td>
+        <td>36</td>
+        <td>48</td>
+        <td>52</td>
+        <td>56</td>
+        <td>60</td>
+        <td>64</td>
+        <td>68</td>
+        <td>72</td>
+        <td>72</td>
+        <td>78</td>
+        <td>84</td>
+        <td>90</td>
+        <td>96</td>
+        <td>102</td>
+        <td>108</td>
+        <td>96</td>
+        <td>104</td>
+        <td>112</td>
+        <td>120</td>
+        <td>128</td>
+        <td>136</td>
+        <td>144</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td rowspan="4">３倍ぬい</td>
+        <td>18</td>
+        <td>20</td>
+        <td>21</td>
+        <td>23</td>
+        <td>24</td>
+        <td>26</td>
+        <td>27</td>
+        <td>36</td>
+        <td>39</td>
+        <td>42</td>
+        <td>45</td>
+        <td>48</td>
+        <td>51</td>
+        <td>54</td>
+        <td>54</td>
+        <td>59</td>
+        <td>63</td>
+        <td>68</td>
+        <td>72</td>
+        <td>77</td>
+        <td>81</td>
+        <td>72</td>
+        <td>78</td>
+        <td>84</td>
+        <td>90</td>
+        <td>96</td>
+        <td>102</td>
+        <td>108</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>36</td>
+        <td>40</td>
+        <td>42</td>
+        <td>46</td>
+        <td>48</td>
+        <td>52</td>
+        <td>54</td>
+        <td>72</td>
+        <td>78</td>
+        <td>84</td>
+        <td>90</td>
+        <td>96</td>
+        <td>102</td>
+        <td>108</td>
+        <td>107</td>
+        <td>118</td>
+        <td>126</td>
+        <td>136</td>
+        <td>144</td>
+        <td>154</td>
+        <td>162</td>
+        <td>144</td>
+        <td>156</td>
+        <td>168</td>
+        <td>180</td>
+        <td>192</td>
+        <td>204</td>
+        <td>216</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+</script>
+
+<style media="screen">
+  th{
+    background: #ccc;
+  }
+  td {
+    border: 1px solid;
+    width: 2em;
+    height: 1em;
+  }
+</style>
